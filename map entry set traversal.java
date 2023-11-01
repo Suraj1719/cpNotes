daa8b9c2f42311bc93a67cb458cb806bfa439d7e
Suraj Kumar Behera <101680569+Suraj1719@users.noreply.github.com>
2023-11-01 09:31:21 +0530

    Create map entry set traversal.java

---
+++ map entry set traversal.java
@@ -0,0 +1,6 @@
+Map<Integer,Integer>val_frq=new HashMap<>();
+
+        for(Map.Entry<Integer,Integer>en:val_frq.entrySet()){
+            int val=en.getValue();
+            int key=en.getKey();
+        }
